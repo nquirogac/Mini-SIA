0d40c5e36947fe12350903df54d5e16a55b16bde
nquirogac <69173681+nquirogac@users.noreply.github.com>
2020-09-17 11:09:03 -0500

    Add files via upload

---
+++ Prueba.java
@@ -0,0 +1,35 @@
+package Logic;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import Data.Asignatura;
+import Data.Carrera;
+import Data.Estudiante;
+import Data.Facultad;
+import Data.Grupo;
+import Data.Nota;
+import Data.Profesor;
+import java.text.ParseException;
+import java.util.Date;
+
+public class Prueba {
+    public static void main(String[] args) throws ParseException {  
+        
+        Carrera carrera1 = new Carrera("Ingeniería de sistemas","Bogotá",2879,165); //
+        Carrera carrera2 = new Carrera("Economía","Bogotá",2578,155); //
+        SimpleDateFormat d = new SimpleDateFormat("dd-MM-yy");
+        Date fecha1 = d.parse("23-09-1981"); 
+        Date fecha2 = d.parse("29-01-1972");
+        
+        Profesor profe1 = new Profesor("Marta","Torres",56938456,"mtorresh",fecha2);
+        Profesor profe2 = new Profesor("Felipe","Castro",68097547,"fcastroh",fecha1);
+        
+        Nota nota1 = new Nota(4.2);
+        Nota nota2 = new Nota(3.7);
+        Nota nota3 = new Nota(4.8);
+        
+        //carrera1.setFacultad(ingenieria);
+        //carrera2.setFacultad(cienciasEconomicas);
+        System.out.println(nota1);
+        
+    }  
+}
