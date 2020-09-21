48ea3f6ef395dcbe03dddfe1e0776b2d9e855325
KevinCRB <70531317+KevinCRB@users.noreply.github.com>
2020-09-20 20:53:04 -0500

    Update Prueba.java

--- Prueba.java
+++ Prueba.java
@@ -1,4 +1,5 @@
 package Logic;
+import java.util.List;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import Data.Asignatura;
@@ -9,11 +10,12 @@ import Data.Grupo;
 import Data.Nota;
 import Data.Profesor;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Prueba {
     public static void main(String[] args) throws ParseException {  
-        
+        List <Carrera> carrerasingenieria= new ArrayList<Carrera>();
         Carrera carrera1 = new Carrera("Ingeniería de sistemas","Bogotá",2879,165); //
         Carrera carrera2 = new Carrera("Economía","Bogotá",2578,155); //
         SimpleDateFormat d = new SimpleDateFormat("dd-MM-yy");
@@ -27,9 +29,24 @@ public class Prueba {
         Nota nota2 = new Nota(3.7);
         Nota nota3 = new Nota(4.8);
         
+        String[] DepartamentosIngenieria={"Sistemas e Industrial","Macanica","Electrica y Electronica"};
+        
+        Facultad ingenieria = new Facultad("Ingenieria",1298373,"Bogota",DepartamentosIngenieria);
+        
+        carrera1.setFacultad(ingenieria);
+        
+        carrerasingenieria.add(carrera1);
+        carrerasingenieria.add(carrera2);
+        
+        ingenieria.setCarrerasFacultad(carrerasingenieria);
+        
+        Asignatura poo = new Asignatura("Programacion Orientada a Objetos",10001029,"Practica",3);
+        
         
         System.out.println(nota1);
         System.out.println(profe1);
+        System.out.println(carrera1);
+        System.out.println(poo);
         
     }  
 }
