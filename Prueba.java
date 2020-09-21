8994eb61c1935b178bd46cbcd3ee5f03c5e12ed5
nquirogac <69173681+nquirogac@users.noreply.github.com>
2020-09-21 11:46:11 -0500

    Update Prueba.java

--- Prueba.java
+++ Prueba.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Prueba {
+    static int opcion;
     public static void main(String[] args) throws ParseException {  
         List <Carrera> carrerasingenieria= new ArrayList<Carrera>();
         Carrera carrera1 = new Carrera("Ingeniería de sistemas","Bogotá",2879,165); //
@@ -48,5 +49,44 @@ public class Prueba {
         System.out.println(carrera1);
         System.out.println(poo);
         
-    }  
+        Interfaz.iniciar();
+        Interfaz.preguntar();
+        opcion = Interfaz.responder();
+        switch (opcion) {
+		case 1:
+                    System.out.println("\tAsignaturas");
+                    break;
+		case 2:
+                    System.out.println("\tCarreras");
+                    System.out.println(carrera1+"\n");
+                    System.out.println(carrera2+"\n");
+                    break;
+                case 3:
+                    System.out.println("\tEstudiantes");
+                    break;
+                case 4:
+                    System.out.println("\tFacultades");
+                    break;
+                case 5: 
+                    System.out.println("\tGrupos");
+                    break;
+                case 6:
+                    System.out.println("\tNotas");
+                    System.out.println(nota1+"\n");
+                    System.out.println(nota2+"\n");
+                    System.out.println(nota3+"\n");
+                    break;
+                case 7:
+                    System.out.println("\tProfesores");
+                    System.out.println(profe1+"\n");
+                    System.out.println(profe2+"\n");
+                    break;
+		default: 
+                    System.out.println("Error");
+        } 
+        
+    }
+    public static int retornarOpcion(){
+        return opcion;
+    }
 }
